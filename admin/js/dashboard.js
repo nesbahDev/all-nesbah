ad7d648ba21af185fe1f9598c35d9ac41e032fde
M. Arif Mardhavi <arifmardhavi@gmail.com>
2022-12-07 10:51:33 +0700

    push all neshbah

---
+++ admin/js/dashboard.js
@@ -0,0 +1,326 @@
+(function($) {
+  'use strict';
+  $(function() {
+    if ($("#leads").length) {
+      var areaData = {
+        labels: ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
+        datasets: [
+          {
+            data: [0.3, 0.1, 0.7, 0.6, 0.68, 0.35, 0.38],
+            borderColor: [
+              '#D43F8D'
+            ],
+            borderWidth: 2,
+            fill: false,
+            label: "leads"
+          }
+        ]
+      };
+      var areaOptions = {
+        responsive: true,
+        maintainAspectRatio: true,
+        plugins: {
+          filler: {
+            propagate: false
+          }
+        },
+        scales: {
+          xAxes: [{
+            display: true,
+            ticks: {
+              display: true,
+              padding: 10,
+              fontColor:"#6C7383"
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false,
+              color: 'transparent',
+              zeroLineColor: '#eeeeee'
+            }
+          }],
+          yAxes: [{
+            display: true,
+            ticks: {
+              display: true,
+              autoSkip: false,
+              maxRotation: 0,
+              stepSize: 0.2,
+              min: 0,
+              max: 1,
+              padding: 18,
+              fontColor:"#6C7383"
+            },
+            gridLines: {
+              display: true,
+              color:"#f2f2f2",
+              drawBorder: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: true
+        },
+        elements: {
+          line: {
+            tension: .35
+          },
+          point: {
+            radius: 0
+          }
+        }
+      }
+      var revenueChartCanvas = $("#leads").get(0).getContext("2d");
+      var revenueChart = new Chart(revenueChartCanvas, {
+        type: 'line',
+        data: areaData,
+        options: areaOptions
+      });
+    }
+
+    if ($("#aplications").length) {
+      var areaData = {
+        labels: ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
+        datasets: [
+          {
+            data: [0, 0, 0, 0, 0, 0, 0],
+            borderColor: [
+              'blue'
+            ],
+            borderWidth: 2,
+            fill: false,
+            label: "aplications"
+          }
+        ]
+      };
+      var areaOptions = {
+        responsive: true,
+        maintainAspectRatio: true,
+        plugins: {
+          filler: {
+            propagate: false
+          }
+        },
+        scales: {
+          xAxes: [{
+            display: true,
+            ticks: {
+              display: true,
+              padding: 10,
+              fontColor:"#6C7383"
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false,
+              color: 'transparent',
+              zeroLineColor: '#eeeeee'
+            }
+          }],
+          yAxes: [{
+            display: true,
+            ticks: {
+              display: true,
+              autoSkip: false,
+              maxRotation: 0,
+              stepSize: 0.2,
+              min: -1,
+              max: 1,
+              padding: 18,
+              fontColor:"#6C7383"
+            },
+            gridLines: {
+              display: true,
+              color:"#f2f2f2",
+              drawBorder: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: true
+        },
+        elements: {
+          line: {
+            tension: .35
+          },
+          point: {
+            radius: 0
+          }
+        }
+      }
+      var revenueChartCanvas = $("#aplications").get(0).getContext("2d");
+      var revenueChart = new Chart(revenueChartCanvas, {
+        type: 'line',
+        data: areaData,
+        options: areaOptions
+      });
+    }
+
+    if ($("#application-status").length) {
+
+      var doughnutPieData = {
+        datasets: [{
+          data: [3, 49],
+          backgroundColor: [
+            '#0774F8',
+            '#D43F8D'
+          ],
+        }],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+          'opened',
+          'pending',
+        ]
+      };
+      var doughnutPieOptions = {
+        responsive: true,
+        animation: {
+          animateScale: true,
+          animateRotate: true
+        }
+      };
+
+      var doughnutChartCanvas = $("#application-status").get(0).getContext("2d");
+      var doughnutChart = new Chart(doughnutChartCanvas, {
+        type: 'doughnut',
+        data: doughnutPieData,
+        options: doughnutPieOptions
+      });
+    }
+
+    if ($("#no_of_applications").length) {
+      var data = {
+        labels: ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
+        datasets: [
+          {
+            data: [3, 10, 7, 16, 6.8, 11, 8],
+            backgroundColor: 'green',
+            width: 10,
+            label: "Total Application"
+          }
+        ]
+      };
+      var options = {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+              max : 18
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        elements: {
+          point: {
+            radius: 0
+          }
+        }
+      };
+      var barChartCanvas = $("#no_of_applications").get(0).getContext("2d");
+      // This will get the first returned node in the jQuery collection.
+      var barChart = new Chart(barChartCanvas, {
+        type: 'bar',
+        data: data,
+        options: options
+      });
+    }
+
+    if ($("#user-growth").length) {
+      var areaData = {
+        labels: ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
+        datasets: [
+          {
+            data: [0.3, 1, 0.7, 0.4, 0.87, 0.35, 0.38],
+            borderColor: [
+              '#D43F8D'
+            ],
+            borderWidth: 2,
+            fill: false,
+            label: "user growth"
+          }
+        ]
+      };
+      var areaOptions = {
+        responsive: true,
+        maintainAspectRatio: true,
+        plugins: {
+          filler: {
+            propagate: false
+          }
+        },
+        scales: {
+          xAxes: [{
+            display: true,
+            ticks: {
+              display: true,
+              padding: 10,
+              fontColor:"#6C7383"
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false,
+              color: 'transparent',
+              zeroLineColor: '#eeeeee'
+            }
+          }],
+          yAxes: [{
+            display: true,
+            ticks: {
+              display: true,
+              autoSkip: false,
+              maxRotation: 0,
+              stepSize: 0.2,
+              min: 0,
+              max: 1,
+              padding: 18,
+              fontColor:"#6C7383"
+            },
+            gridLines: {
+              display: true,
+              color:"#f2f2f2",
+              drawBorder: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: true
+        },
+        elements: {
+          line: {
+            tension: .35
+          },
+          point: {
+            radius: 0
+          }
+        }
+      }
+      var revenueChartCanvas = $("#user-growth").get(0).getContext("2d");
+      var revenueChart = new Chart(revenueChartCanvas, {
+        type: 'line',
+        data: areaData,
+        options: areaOptions
+      });
+    }
+
+    function format ( d ) {
+      // `d` is the original data object for the row
+      return '<table cellpadding="5" cellspacing="0" border="0" style="width:100%;">'+
+          '<tr class="expanded-row">'+
+              '<td colspan="8" class="row-bg"><div><div class="d-flex justify-content-between"><div class="cell-hilighted"><div class="d-flex mb-2"><div class="mr-2 min-width-cell"><p>Policy start date</p><h6>25/04/2020</h6></div><div class="min-width-cell"><p>Policy end date</p><h6>24/04/2021</h6></div></div><div class="d-flex"><div class="mr-2 min-width-cell"><p>Sum insured</p><h5>$26,000</h5></div><div class="min-width-cell"><p>Premium</p><h5>$1200</h5></div></div></div><div class="expanded-table-normal-cell"><div class="mr-2 mb-4"><p>Quote no.</p><h6>Incs234</h6></div><div class="mr-2"><p>Vehicle Reg. No.</p><h6>KL-65-A-7004</h6></div></div><div class="expanded-table-normal-cell"><div class="mr-2 mb-4"><p>Policy number</p><h6>Incsq123456</h6></div><div class="mr-2"><p>Policy number</p><h6>Incsq123456</h6></div></div><div class="expanded-table-normal-cell"><div class="mr-2 mb-3 d-flex"><div class="highlighted-alpha"> A</div><div><p>Agent / Broker</p><h6>Abcd Enterprices</h6></div></div><div class="mr-2 d-flex"> <img src="../../images/faces/face5.jpg" alt="profile"/><div><p>Policy holder Name & ID Number</p><h6>Phillip Harris / 1234567</h6></div></div></div><div class="expanded-table-normal-cell"><div class="mr-2 mb-4"><p>Branch</p><h6>Koramangala, Bangalore</h6></div></div><div class="expanded-table-normal-cell"><div class="mr-2 mb-4"><p>Channel</p><h6>Online</h6></div></div></div></div></td>'
+          '</tr>'+
+      '</table>';
+  }
+  
+  });
+})(jQuery);
